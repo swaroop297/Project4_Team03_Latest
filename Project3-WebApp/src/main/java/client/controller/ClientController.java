a64b95868d4d60b1e780501d0f90d26d8b3843d1
Himanshu Dua <32210683+hdua1@users.noreply.github.com>
2018-04-20 14:51:21 -0700

    Update ClientController.java

--- Project3-WebApp/src/main/java/client/controller/ClientController.java
+++ Project3-WebApp/src/main/java/client/controller/ClientController.java
@@ -1,33 +1,22 @@
 package client.controller;
 
 import java.awt.event.WindowEvent;
-
 import client.view.ClientUi;
+
 /**
- * Main Controller that invokes the client UI
- * @SER516 Project3_Team03
- * @version 1.0
+ * Main Controller that invokes the client UI including running & closing
  */
 public class ClientController {
-	static ClientUi clientui;
+	static ClientUi clientUI;
 	ExpressiveController expressiveController;
 	AffectiveController affectiveController;
-	
-	/**
-	 * Run the client UI.
-	 */
 	public ClientController() {
 		expressiveController = new ExpressiveController();
 		affectiveController = new AffectiveController();
-		clientui = new ClientUi(expressiveController, affectiveController);
-		clientui.setVisible(true);
-
+		clientUI = new ClientUi(expressiveController, affectiveController);
+		clientUI.setVisible(true);
 	}
-	
-	/**
-	 * Closes the client UI.
-	 */
 	public static void close(){
-		clientui.dispatchEvent(new WindowEvent(clientui, WindowEvent.WINDOW_CLOSING));
+		clientui.dispatchEvent(new WindowEvent(clientUI, WindowEvent.WINDOW_CLOSING));
 	}
 }
