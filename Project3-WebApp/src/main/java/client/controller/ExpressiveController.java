9d169a76318f863ba5ef764598977e976400911b
Himanshu Dua <32210683+hdua1@users.noreply.github.com>
2018-04-20 14:56:17 -0700

    Update ExpressiveController.java

--- Project3-WebApp/src/main/java/client/controller/ExpressiveController.java
+++ Project3-WebApp/src/main/java/client/controller/ExpressiveController.java
@@ -2,10 +2,8 @@ package client.controller;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import client.view.DisplayGraph;
 import client.view.ExpressiveView;
 import client.view.FacePaint;
@@ -14,39 +12,36 @@ import utility.FaceExpressionData;
 
 /**
  * Controller for the Expressions View
- * 
- * @SER516 Project3_Team03
- * @version 1.0
  */
 public class ExpressiveController {
 	public ExpressiveView expressiveView;
 	JPanel facePanel;
 	JPanel graphPanel;
 	private JPanel blinkGraph;
-	private JPanel rgtWinkGraph;
-	private JPanel lftWinkGraph;
-	private JPanel looklftGraph;
-	private JPanel lookrgtGraph;
+	private JPanel rightWinkGraph;
+	private JPanel leftWinkGraph;
+	private JPanel lookLeftGraph;
+	private JPanel lookRightGraph;
 	private JPanel furrowGraph;
 	private JPanel raiseGraph;
 	private JPanel smileGraph;
-	private JPanel lftSmirkGraph;
-	private JPanel rgtSmirkGraph;
+	private JPanel leftSmirkGraph;
+	private JPanel rightSmirkGraph;
 	private JPanel clenchGraph;
 	private JPanel laughGraph;
-	private FacePaint f;
-	private DisplayGraph g;
-	private DisplayGraph g1;
-	private DisplayGraph g2;
-	private DisplayGraph g3;
-	private DisplayGraph g4;
-	private DisplayGraph g5;
-	private DisplayGraph g6;
-	private DisplayGraph g7;
-	private DisplayGraph g8;
-	private DisplayGraph g9;
-	private DisplayGraph g10;
-	private DisplayGraph g11;
+	private FacePaint face;
+	private DisplayGraph graph;
+	private DisplayGraph graph1;
+	private DisplayGraph graph2;
+	private DisplayGraph graph3;
+	private DisplayGraph graph4;
+	private DisplayGraph graph5;
+	private DisplayGraph graph6;
+	private DisplayGraph graph7;
+	private DisplayGraph graph8;
+	private DisplayGraph graph9;
+	private DisplayGraph graph10;
+	private DisplayGraph graph11;
 	Thread t;
 	double v[]= new double[13];
 	boolean flag=true;
@@ -56,21 +51,21 @@ public class ExpressiveController {
 		facePanel = new JPanel();
 		graphPanel = new JPanel();
 		blinkGraph = new JPanel();
-		rgtWinkGraph = new JPanel();
-		lftWinkGraph = new JPanel();
-		looklftGraph = new JPanel();
-		lookrgtGraph = new JPanel();
+		rightWinkGraph = new JPanel();
+		leftWinkGraph = new JPanel();
+		lookLeftGraph = new JPanel();
+		lookRightGraph = new JPanel();
 		furrowGraph = new JPanel();
 		raiseGraph = new JPanel();
 		smileGraph = new JPanel();
 		clenchGraph = new JPanel();
-		lftSmirkGraph = new JPanel();
+		leftSmirkGraph = new JPanel();
 		smileGraph = new JPanel();
-		rgtSmirkGraph = new JPanel();
+		rightSmirkGraph = new JPanel();
 		laughGraph = new JPanel();
 
-		expressiveView = new ExpressiveView(facePanel, graphPanel, blinkGraph, rgtWinkGraph, lftWinkGraph, looklftGraph,
-				lookrgtGraph, furrowGraph, raiseGraph, smileGraph, clenchGraph, lftSmirkGraph, rgtSmirkGraph,
+		expressiveView = new ExpressiveView(facePanel, graphPanel, blinkGraph, rightWinkGraph, letWinkGraph, lookLeftGraph,
+				lookRightGraph, furrowGraph, raiseGraph, smileGraph, clenchGraph, leftSmirkGraph, rightSmirkGraph,
 				laughGraph);
 
 		final JLabel faceLabel = new JLabel("    Face Expression");
@@ -93,62 +88,62 @@ public class ExpressiveController {
 		graphLabel.setBounds(336, 0, 357, 29);
 		expressiveView.add(graphLabel);
 		
-		f = new FacePaint();
-		g = new DisplayGraph();
-		g1 = new DisplayGraph();
-		g2 = new DisplayGraph();
-		g3 = new DisplayGraph();
-		g4 = new DisplayGraph();
-		g5 = new DisplayGraph();
-		g6 = new DisplayGraph();
-		g7 = new DisplayGraph();
-		g8 = new DisplayGraph();
-		g9 = new DisplayGraph();
-		g10 = new DisplayGraph();
-		g11 = new DisplayGraph();
+		face = new FacePaint();
+		graph = new DisplayGraph();
+		graph1 = new DisplayGraph();
+		graph2 = new DisplayGraph();
+		graph3 = new DisplayGraph();
+		graph4 = new DisplayGraph();
+		graph5 = new DisplayGraph();
+		graph6 = new DisplayGraph();
+		graph7 = new DisplayGraph();
+		graph8 = new DisplayGraph();
+		graph9 = new DisplayGraph();
+		graph10 = new DisplayGraph();
+		graph11 = new DisplayGraph();
 		
-		facePanel.add(f, BorderLayout.CENTER);
-		blinkGraph.add(g, BorderLayout.CENTER);
-		rgtWinkGraph.add(g1, BorderLayout.CENTER);
-		lftWinkGraph.add(g2, BorderLayout.CENTER);
-		lookrgtGraph.add(g3, BorderLayout.CENTER);
-		looklftGraph.add(g4, BorderLayout.CENTER);
-		furrowGraph.add(g5, BorderLayout.CENTER);
-		raiseGraph.add(g6, BorderLayout.CENTER);
-		smileGraph.add(g7, BorderLayout.CENTER);
-		clenchGraph.add(g8, BorderLayout.CENTER);
-		lftSmirkGraph.add(g9, BorderLayout.CENTER);
-		rgtSmirkGraph.add(g10, BorderLayout.CENTER);
-		laughGraph.add(g11, BorderLayout.CENTER);
+		facePanel.add(face, BorderLayout.CENTER);
+		blinkGraph.add(graph, BorderLayout.CENTER);
+		rightWinkGraph.add(graph1, BorderLayout.CENTER);
+		leftWinkGraph.add(graph2, BorderLayout.CENTER);
+		lookRightGraph.add(graph3, BorderLayout.CENTER);
+		lookLeftGraph.add(graph4, BorderLayout.CENTER);
+		furrowGraph.add(graph5, BorderLayout.CENTER);
+		raiseGraph.add(graph6, BorderLayout.CENTER);
+		smileGraph.add(graph7, BorderLayout.CENTER);
+		clenchGraph.add(graph8, BorderLayout.CENTER);
+		leftSmirkGraph.add(graph9, BorderLayout.CENTER);
+		rightSmirkGraph.add(graph10, BorderLayout.CENTER);
+		laughGraph.add(graph11, BorderLayout.CENTER);
 		
 		//Thread to make sure that the graphs keeps plotting the points.
 		t = new Thread(new Runnable() {
 			public void run() {
 				while (flag) {
-					g.addValues(v[8]);
-					g1.addValues(v[10]);
-					g2.addValues(v[9]);
-					g3.addValues(v[12]);
-					g4.addValues(v[11]);
-					g5.addValues(v[2]);
-					g6.addValues(v[1]);
-					g7.addValues(v[3]);
-					g8.addValues(v[4]);
-					g9.addValues(v[5]);
-					g10.addValues(v[6]);
-					g11.addValues(v[7]);
+					graph.addValues(v[8]);
+					graph1.addValues(v[10]);
+					graph2.addValues(v[9]);
+					graph3.addValues(v[12]);
+					graph4.addValues(v[11]);
+					graph5.addValues(v[2]);
+					graph6.addValues(v[1]);
+					graph7.addValues(v[3]);
+					graph8.addValues(v[4]);
+					graph9.addValues(v[5]);
+					graph10.addValues(v[6]);
+					graph11.addValues(v[7]);
 					facePanel.repaint();
 					blinkGraph.repaint();
-					rgtWinkGraph.repaint();
-					lftWinkGraph.repaint();
-					lookrgtGraph.repaint();
-					looklftGraph.repaint();
+					rightWinkGraph.repaint();
+					leftWinkGraph.repaint();
+					lookRightGraph.repaint();
+					lookLeftGraph.repaint();
 					furrowGraph.repaint();
 					raiseGraph.repaint();
 					smileGraph.repaint();
 					clenchGraph.repaint();
-					lftSmirkGraph.repaint();
-					rgtSmirkGraph.repaint();
+					leftSmirkGraph.repaint();
+					rightSmirkGraph.repaint();
 					laughGraph.repaint();
 					try {
 				 		Thread.sleep(1000);
@@ -157,11 +152,9 @@ public class ExpressiveController {
 				 	}
 				}
 			}
-		
 		 });
-
 	}
-	
+
 	/**
 	 * Updates the Face and the Graph on the basis of received values
 	 * @param fed Contains the latest expression values
@@ -169,19 +162,19 @@ public class ExpressiveController {
 	public void updateGraph(FaceExpressionData fed) {
 		flag = false;
 		v = fed.fetchVectors();
-		f.changeVector(v);
-		g.addValues(v[8]);
-		g1.addValues(v[10]);
-		g2.addValues(v[9]);
-		g3.addValues(v[12]);
-		g4.addValues(v[11]);
-		g5.addValues(v[2]);
-		g6.addValues(v[1]);
-		g7.addValues(v[3]);
-		g8.addValues(v[4]);
-		g9.addValues(v[5]);
-		g10.addValues(v[6]);
-		g11.addValues(v[7]);
+		face.changeVector(v);
+		graph.addValues(v[8]);
+		graph1.addValues(v[10]);
+		graph2.addValues(v[9]);
+		graph3.addValues(v[12]);
+		graph4.addValues(v[11]);
+		graph5.addValues(v[2]);
+		graph6.addValues(v[1]);
+		graph7.addValues(v[3]);
+		graph8.addValues(v[4]);
+		graph9.addValues(v[5]);
+		graph10.addValues(v[6]);
+		graph11.addValues(v[7]);
 		flag = true;
 		try{
 			t.start();
@@ -197,5 +190,4 @@ public class ExpressiveController {
 	public void updateTime(FaceData data){
 		timeValue.setText(data.getTimeElapsed().toString());
 	}
-
 }
