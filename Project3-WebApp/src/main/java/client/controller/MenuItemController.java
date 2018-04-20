c5f85e5b973a080937db17470eeb0286bf7d6d27
Himanshu Dua <32210683+hdua1@users.noreply.github.com>
2018-04-20 14:57:39 -0700

    Update MenuItemController.java

--- Project3-WebApp/src/main/java/client/controller/MenuItemController.java
+++ Project3-WebApp/src/main/java/client/controller/MenuItemController.java
@@ -2,32 +2,23 @@ package client.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-
 import client.service.FaceClient;
 import server.ServerMainHandler;
 
 /**
- * Controller for the menu items
- * 
- * @SER516 Project3_Team03
- * @Version 1.0
+ * Controller for the menu items used to connect to the server, start the server & close the application.
  */
 public class MenuItemController {
-
 	public MenuItemController(JMenuItem serverConnect, JMenuItem serverConsole, JMenuItem exit) {
-		
-		//Connects to the server.
 		serverConnect.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				JTextField host = new JTextField();
 				host.setText("localhost");
 				JTextField port = new JTextField();
 				port.setText("8000");
-
 				Object[] fields = { "Host Name", host, "Port", port };
 				int input = JOptionPane.showConfirmDialog(null, fields, "Connect To Server",
 						JOptionPane.OK_CANCEL_OPTION);
@@ -42,20 +33,15 @@ public class MenuItemController {
 				}
 			}
 		});
-		
-		//Closes the application.
 		exit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ClientController.close();
 			}
 		});
-		
-		//Starts the server.
 		serverConsole.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				new ServerMainHandler();
 			}
 		});
 	}
-
 }
