2135f3fbb7d1e1156649f85d028c75a09abf95f2
Himanshu Dua <32210683+hdua1@users.noreply.github.com>
2018-04-20 14:48:30 -0700

    Update AffectiveController.java

--- Project3-WebApp/src/main/java/client/controller/AffectiveController.java
+++ Project3-WebApp/src/main/java/client/controller/AffectiveController.java
@@ -6,20 +6,15 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import utility.FaceAffectiveData;
 
-
 /**
  * Controller for the Affective View
- * 
- * @SER516 Project3_Team03
- * @version 1.0
  */
 public class AffectiveController {
-
 	public AffectiveView affectiveView;
 	JPanel leftPanel;
 	private JPanel affectiveGraph;
-	JPanel perfPanel;
-	private AffectiveDisplayGraph g1;
+	JPanel performancePanel;
+	private AffectiveDisplayGraph graph1;
 	boolean flag=true;
 	Thread t;
 	double v[]= new double[6];
@@ -27,16 +22,16 @@ public class AffectiveController {
 	public AffectiveController() {
 		leftPanel = new JPanel();
 		affectiveGraph = new JPanel();
-		perfPanel = new JPanel();
+		performancePanel = new JPanel();
 
-		affectiveView = new AffectiveView(leftPanel, affectiveGraph, perfPanel);
-		g1 = new AffectiveDisplayGraph();
-		affectiveGraph.add(g1, BorderLayout.CENTER);
+		affectiveView = new AffectiveView(leftPanel, affectiveGraph, performancePanel);
+		graph1 = new AffectiveDisplayGraph();
+		affectiveGraph.add(graph1, BorderLayout.CENTER);
 		
 		t = new Thread(new Runnable() {
 			public void run() {
 				while (flag) {
-					g1.addValues(v);
+					graph1.addValues(v);
 					affectiveGraph.repaint();
 					try {
 				 		Thread.sleep(1000);
@@ -45,19 +40,17 @@ public class AffectiveController {
 				 	}
 				}
 			}
-		
 		 });
-
 	}
 	
 	/**
 	 * Updates Graph on the basis of received values
-	 * @param fed Contains the latest affective values
+	 * @param fed contains the latest affective values
 	 */
 	public void updateGraph(FaceAffectiveData fed) {
 		flag = false;
 		v = fed.fetchVectors();
-		g1.addValues(v);
+		graph1.addValues(v);
 		flag = true;
 		try{
 			t.start();
@@ -65,5 +58,4 @@ public class AffectiveController {
 			
 		}
 	}
-
 }
