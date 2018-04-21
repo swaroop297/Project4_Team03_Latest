751431a24431a4ff13ef3eb3512813a1f18dcbbe
Shubhangi Gupta <shubhangi140793@gmail.com>
2018-04-20 20:05:06 -0400

    added functionality for dowloading csv

--- Project3-WebApp/src/main/java/utility/FaceData.java
+++ Project3-WebApp/src/main/java/utility/FaceData.java
@@ -1,7 +1,12 @@
 package utility;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 /**
  * Utility class containing the face data.
+ * 
  * @SER516 Project3_Team03
  * @version 1.0
  */
@@ -9,33 +14,63 @@ public class FaceData {
 	FaceExpressionData faceExpressionData;
 	FaceAffectiveData faceAffectiveData;
 	Double timeElapsed = 0.0;
-	
+
 	public FaceExpressionData getFaceExpressionData() {
 		return faceExpressionData;
 	}
+
 	public void setFaceExpressionData(FaceExpressionData faceExpressionData) {
 		this.faceExpressionData = faceExpressionData;
 	}
+
 	public FaceAffectiveData getFaceAffectiveData() {
 		return faceAffectiveData;
 	}
+
 	public void setFaceAffectiveData(FaceAffectiveData faceAffectiveData) {
 		this.faceAffectiveData = faceAffectiveData;
-	}	
+	}
+
 	public Double getTimeElapsed() {
 		return timeElapsed;
 	}
+
 	public void setTimeElapsed(Double timeElapsed) {
 		this.timeElapsed = timeElapsed;
 	}
-	
+
 	/**
 	 * Converts the received affective data into string.
 	 */
 	public String toString() {
-		return "FaceData{" +
-				"faceExpressionData=" + faceExpressionData +
-				", faceAffectiveData=" + faceAffectiveData +
-				'}';
+		return "FaceData{" + "faceExpressionData=" + faceExpressionData + ", faceAffectiveData=" + faceAffectiveData
+				+ '}';
+	}
+
+	public void tocsv() {
+		PrintWriter pw;
+		try {
+			pw = new PrintWriter(new File(System.getProperty("user.home") + "/Downloads/FaceExpressiondata.csv"));
+			StringBuilder sb = new StringBuilder();
+			sb.append("Raise Brow ,Furrow Brow ,Smile,Clench,Smirk Left,Smirk Right, laugh, blink, "
+					+ "Wink Left, Wink Right, Look Left, Look Right, Eye Reset, Meditation, Engagement Boredom,"
+					+ "Frustation, Excitement ShortTerm,Excitement LongTerm, Time Elapsed");
+			sb.append('\n');
+			sb.append(faceExpressionData.getRaiseBrow() + "," + faceExpressionData.getFurrowBrow() + ","
+					+ faceExpressionData.getSmile() + "," + faceExpressionData.getClench() + ","
+					+ faceExpressionData.getSmirkLeft() + "," + faceExpressionData.getSmirkRight() + ","
+					+ faceExpressionData.getLaugh() + "," + faceExpressionData.getBlink() + ","
+					+ faceExpressionData.getWinkLeft() + "," + faceExpressionData.getWinkRight() + ","
+					+ faceExpressionData.getLookLeft() + "," + faceExpressionData.getLookRight() + ","
+					+ faceExpressionData.getEyeReset() + "," + faceAffectiveData.getMeditation() + ","
+					+ faceAffectiveData.getEngagementBoredom() + "," + faceAffectiveData.getFrustation() + ","
+					+ faceAffectiveData.getExcitementShortTerm() + "," + faceAffectiveData.getExcitementLongTerm() + ","
+					+ getTimeElapsed());
+			pw.write(sb.toString());
+			pw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.println("done!");
 	}
 }
