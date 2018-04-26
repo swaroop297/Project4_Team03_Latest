6707b64f18930862f75e6b794a5456d9b25aff44
Himanshu Dua <32210683+hdua1@users.noreply.github.com>
2018-04-25 17:07:12 -0700

    Update MenuController.java

--- Project3-WebApp/src/main/java/server/controller/MenuController.java
+++ Project3-WebApp/src/main/java/server/controller/MenuController.java
@@ -2,38 +2,27 @@ package server.controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
-
 import server.ServerMainHandler;
 
 /**
  * Controller for the menu items
- * 
- * @SER516 Project3_Team03
- * @Version 1.0
+ * Gives information about the application
+ * Closes the application
  */
 public class MenuController {
-
 	public MenuController(JMenuItem about, JMenuItem exit ){
-		
-		//Gives information about the application
 		about.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent arg0) {
-				// TODO Auto-generated method stub
 				JOptionPane.showMessageDialog(null, "Emoticon Application. Version 1.0");
 			}
 		});
 		
-		//CLoses the application
 		exit.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent e) {
 				ServerMainHandler.close();
 			}
 		});
 	}
-	
 }
